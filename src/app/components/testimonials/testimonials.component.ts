eba144f76b95ff2d55cb8fdbc1409a87a865d6e2
sarahboutej <38035752+sarahboutej@users.noreply.github.com>
2021-07-24 17:44:28 +0100

    Tripedeo-user: Add testmonials section (#3)

--- src/app/components/testimonials/testimonials.component.ts
+++ src/app/components/testimonials/testimonials.component.ts
@@ -6,10 +6,66 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./testimonials.component.scss']
 })
 export class TestimonialsComponent implements OnInit {
+  slideOptions = {
+    loop: true,
+    mouseDrag: false,
+    touchDrag: false,
+    pullDrag: false,
+    dots: false,
+    navSpeed: 700,
+    navText: ['<i class="bi bi-chevron-left"></i>', '<i class="bi bi-chevron-right"></i>'],
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 2
+      },
+      740: {
+        items: 3
+      },
+
+    },
+    nav: true
+  };
+
+  testimonialsItems = [
+    {
+      id: '1',
+      avatar: 'assets/images/user1.jpg',
+      userName: 'Florian B',
+      title: 'Super Easy',
+      stars: 5,
+      description: 'Die Suche und das Gespräch mit dem idealen Reiseexperten für unsere Reise war super einfach und bequem, habe mich noch nie in besseren Händen gefühlt.',
+      userInfo: 'Kreuzfahrt, Ostsee'
+    },
+    {
+      id: '2',
+      avatar: 'assets/images/user2.jpg',
+      userName: 'Florian B',
+      title: 'Unvergessliches Erlebnis',
+      stars: 5,
+      description: 'Unsere Familie hatte einen tollen Urlaub in Südafrika, den wir nie vergessen werden.',
+      userInfo: 'Pauschalurlaub, Südafrika'
+    },
+    {
+      id: '3',
+      avatar: 'assets/images/user3.jpg',
+      userName: 'Florian B',
+      title: 'Großartige Beratung!',
+      stars: 5,
+      description: 'Unser Reiseexperte kannte sich mit Wander- und Radtouren sehr gut aus und hat eine perfekte Reise für uns geplant!',
+      userInfo: 'Wandern und Radfahren, Griechenland'
+    },
+  ];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  counter(i: number) {
+    return new Array(i);
   }
 
 }
