ad83b1c0833e3265036c199aebc7b385a7ed49db
sarahboutej <38035752+sarahboutej@users.noreply.github.com>
2021-11-03 12:26:41 +0100

    Ts-867: Fix availability display (#140)

--- src/app/components/agent-profile-view/availability/availability.component.ts
+++ src/app/components/agent-profile-view/availability/availability.component.ts
@@ -11,9 +11,21 @@ export class AvailabilityComponent implements OnInit {
   @Input()
   agentAvailability: any = [];
 
+  agentAvailabilityDays: any = [];
+
   constructor() { }
 
   ngOnInit(): void {
+  var availabilityDays = this.agentAvailability.reduce(function (r:any, a:any) {
+        r[a.agentAvailabilityDay] = r[a.agentAvailabilityDay] || [];
+        r[a.agentAvailabilityDay].push({
+          "day": a.agentAvailabilityDay,
+          "start": a.agentAvailabilityStartDate,
+          "end":a.agentAvailabilityEndDate
+        });
+        return r;
+    }, Object.create(null));
+    this.agentAvailabilityDays = Object.values(availabilityDays);
+    console.warn(this.agentAvailabilityDays);
   }
-
 }
