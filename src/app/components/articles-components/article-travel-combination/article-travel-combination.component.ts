32afae8ac70dbf28055cd8cef5c2d23583562f0e
Montassar Attia <montassar.attia2@gmail.com>
2021-11-14 19:03:17 +0100

    feat: TS-832

--- src/app/components/articles-components/article-travel-combination/article-travel-combination.component.ts
+++ src/app/components/articles-components/article-travel-combination/article-travel-combination.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'article-travel-combination',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./article-travel-combination.component.scss']
 })
 export class ArticleTravelCombinationComponent implements OnInit {
-
+  @Input() data : any;
   constructor() { }
 
   ngOnInit(): void {
