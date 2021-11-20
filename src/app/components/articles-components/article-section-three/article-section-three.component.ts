b3c4db06afd612ea87ac430a388fe46314c00f00
abir94 <abir_lahmer1994@hotmail.fr>
2021-11-20 23:27:46 +0100

    fix issue TS-927

--- src/app/components/articles-components/article-section-three/article-section-three.component.ts
+++ src/app/components/articles-components/article-section-three/article-section-three.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-article-section-three',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./article-section-three.component.scss']
 })
 export class ArticleSectionThreeComponent implements OnInit {
-
+  @Input() data : any;
   constructor() { }
 
   ngOnInit(): void {
